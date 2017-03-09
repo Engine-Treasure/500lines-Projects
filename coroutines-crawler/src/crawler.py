5f4ec8d915fc4ccf920c8df06bbc4261a98678ea
Engine <enginechen07@gmail.com>
2017-03-09 20:52:15 +0800

    coroutines crawler, part 1

--- coroutines-crawler/src/crawler.py
+++ coroutines-crawler/src/crawler.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
 
 
-
 import socket
 
-q = []
+from selectors import DefaultSelector, EVENT_WRITE, EVENT_READ
+
+urls_todo = set(["/"])
+seen_urls = set(["/"])
+
+stopped = False
 
 
 def parse_links(response):
@@ -12,8 +16,6 @@ def parse_links(response):
 
 
 def fetch(url):
-    from selectors import DefaultSelector, EVENT_WRITE
-
     selector = DefaultSelector()  # Choose best select-like function on system
 
     sock = socket.socket()
@@ -39,7 +41,6 @@ def fetch(url):
                 callback = event_key.data  # 执行等待事件发生的回调函数
                 callback()  # 未完成的操作等到未来的事件循环的某个时钟来到再执行
 
-
     request = "GET {} HTTP/1.0\r\nHost: xkcd.com\r\n\r\n".format(url)
     encoded = request.encode("ascii")
 
@@ -57,4 +58,74 @@ def fetch(url):
         chunk = sock.recv(4096)
 
     links = parse_links(response)
-    q.add(links)
+    seen_urls.add(links)
+
+
+class Fetcher:
+    def __init__(self, url):
+        self.response = b''
+        self.url = url
+        self.sock = None
+
+    def fetch(self):
+        '''
+        The method returns before the connection is established.
+        It must return control to the event loop to wait for the connection.
+        :return:
+        '''
+        sock = socket.socket()
+        sock.setblocking(False)  # by default, socket ops are blocking
+
+        try:  # a non-blocking socket throws an exception from connect, even when it is working normally
+            sock.connect(("xkcd.com", 80))
+        except BlockingIOError:
+            pass
+
+        # Register next callback
+        selector.register(self.sock.fileno(),
+                          EVENT_WRITE,
+                          self.connected)
+
+    def connected(self, key, mask):
+        print("Connected~")
+        selector.unregister(key.fd)
+        request = "GET {} HTTP/1.0\r\nHost: xkcd.com\r\n\r\n".format(self.url)
+        self.sock.send(request.encode("ascii"))
+
+        selector.register(key.fd,
+                          EVENT_READ,
+                          self.read_response)
+
+
+    def read_response(self, key, mask):
+        global stopped
+
+        # 此处没有循环. 如果数据没有读完, socket 将保持可读状态,
+        # 事件循环的下一个时钟到来时, 继续读取
+        # 数据读取完毕, 服务器会关闭 socket, 读出的 chunk 将为空
+        chunk = self.sock.recv(4096)
+        if chunk:
+            self.response += chunk
+        else:
+            selector.unregister(key.fd)  # Done reading.
+            links = self.parse_links()
+
+            for link in links.difference(seen_urls):
+                urls_todo.add(link)
+                Fetcher(link).fetch()  # New Fetcher
+
+            seen_urls.update(links)
+            urls_todo.remove(self.url)
+            if not urls_todo:
+                stopped = True
+
+
+if __name__ == '__main__':
+    fetcher = Fetcher("/353/")
+    fetcher.fetch()
+
+    while True:
+        events = selector.select()
+        for event_key, event_mask in events:
+            callback = event_key.data
+            callback(event_key, event_maskj)
